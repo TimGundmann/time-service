c873a3e79ad3719e769de99e14e9e07605d896e9
Tim Gundmann <tim@gundmann.dk>
2019-12-26 17:23:59 +0100

    query update

--- src/main/java/dk/gundmann/time/RegistrationRepository.java
+++ src/main/java/dk/gundmann/time/RegistrationRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 
 interface RegistrationRepository extends CrudRepository<Registration, String> {
 
-    @Query("SELECT r.startTime FROM REGISTRATION r WHERE r.email = :email and r.endTime is null")
+    @Query("SELECT r.startTime FROM Registration r WHERE r.email = :email and r.endTime is null")
 	Optional<LocalDateTime> findActiveByEmail(String email);
 
 }
